5c4d9473ec15eee137eccfacce1defcf70fb6439
attakei <attakei@gmail.com>
2014-08-15 18:34:49 +0900

    modify initialize logic, declare-rule.

--- tests/test_twitterapi.py
+++ tests/test_twitterapi.py
@@ -15,47 +15,36 @@ class TweetTests(unittest.TestCase):
 
 class ConvertPatternTests(unittest.TestCase):
     class Target(object):
-        __slot__ = ['id_str']
+        id_str = _ConvertPattern('id_str', 'id', str)
 
     def test_convert(self):
         p_ = _ConvertPattern('id_str', 'id', str)
-        self.assertIsInstance(p_.converter(1), str)
+        self.assertIsInstance(p_.convert({'id':1}), str)
 
-    def test_junction(self):
-        p_ = _ConvertPattern('id_str', 'id', str)
-        obj_ = self.Target()
-        data_ = {'id': 111}
-        p_.junction(obj_, data_)
-        self.assertEqual(obj_.id_str, '111')
-
-    def test_junction_no_converter(self):
+    def test_no_convert(self):
         p_ = _ConvertPattern('id_str', 'id')
-        obj_ = self.Target()
         data_ = {'id': 111}
-        p_.junction(obj_, data_)
-        self.assertEqual(obj_.id_str, 111)
+        value = p_.convert(data_)
+        self.assertEqual(value, 111)
 
     def test_convert_failed(self):
         def _strptime(bs):
             return datetime.datetime.strptime(bs, '%a')
         p_ = _ConvertPattern('id_str', 'id', _strptime)
-        obj_ = self.Target()
         data_ = {'id': 111}
         with self.assertRaises(p_.ConvertFailed):
-            p_.junction(obj_, data_)
+            p_.convert(data_)
 
     def test_required(self):
         p_ = _ConvertPattern('id_str', 'id', str)
-        obj_ = self.Target()
         data_ = {'id2': 111}
         with self.assertRaises(p_.RequiredNotFound):
-            p_.junction(obj_, data_)
+            p_.convert(data_)
 
     def test_not_required(self):
         p_ = _ConvertPattern('id_str', 'id', str, False)
-        obj_ = self.Target()
         data_ = {'id2': 111}
         try:
-            p_.junction(obj_, data_)
+            p_.convert(data_)
         except p_.RequiredNotFound:
             self.fail('caught Exception')
--- twinkerer/twitterapi.py
+++ twinkerer/twitterapi.py
@@ -26,14 +26,14 @@ class _ConvertPattern(object):
         self.converter = converter
         self.required = required
 
-    def junction(self, target_obj, base_dict):
+    def convert(self, base_dict):
         if self.origin in base_dict:
             try:
                 if self.converter:
                     attr_value_ = self.converter(base_dict[self.origin])
                 else:
                     attr_value_ = base_dict[self.origin]
-                setattr(target_obj, self.target, attr_value_)
+                return attr_value_
             except:
                 raise self.ConvertFailed()
         elif self.required:
@@ -43,15 +43,11 @@ class _ConvertPattern(object):
 class Tweet(object):
     """Tweet object based from twitter-api json
     """
-    __slots__ = ['id', 'created_at', 'text', ]
-
-    CONVERT_PATTERNS = (
-        _ConvertPattern('id', 'id_str'),
-        _ConvertPattern('created_at', 'created_at', _strptime),
-        _ConvertPattern('text', 'text'),
-    )
-    """convert patterns"""
+    id = _ConvertPattern('id', 'id_str')
+    created_at = _ConvertPattern('created_at', 'created_at', _strptime)
+    text = _ConvertPattern('text', 'text')
 
     def __init__(self, json):
-        for pattern in self.CONVERT_PATTERNS:
-            pattern.junction(self, json)
+        for name_, pattern_ in self.__class__.__dict__.items():
+            if isinstance(pattern_, _ConvertPattern):
+                setattr(self, name_, pattern_.convert(json))
