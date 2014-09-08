1fac836090909764d571057946912f6f3f4986a3
attakei <attakei@gmail.com>
2014-09-08 12:00:44 +0900

    add test code.

---
+++ tests/test_utils.py
@@ -0,0 +1,10 @@
+import unittest
+from twinkerer import utils
+import datetime
+
+
+class StrptimeTests(unittest.TestCase):
+    def test_it(self):
+        dt_ = utils.strptime('Sat Sep 06 16:45:01 +0000 2014')
+        self.assertIsInstance(dt_, datetime.datetime)
+        self.assertNotEqual(dt_.tzinfo, None)
