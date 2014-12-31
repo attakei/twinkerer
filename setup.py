017aa4aebc0c38b026832cf52f2a31ade1de48cb
attakei <attakei@gmail.com>
2014-12-31 15:31:06 +0900

    ver 0.1.0

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ class PyTest(TestCommand):
 
 setup(
     name='twinkerer',
-    version='0.0.2',
+    version='0.1.0',
     description="tinkerer small extension to use twitter api.",
     long_description=readme,
     author='attakei',
